1f105363838a648821266eaa3c698ced3382b9b6
Paul Ashour <paul.ashour@gmail.com>
2018-09-24 15:24:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function takeANumber(curLine, newName) {
   curLine.push(newName);
-  return `Welcome, ${newName}. You are ${curLin
+  return `Welcome, ${newName}. You are number ${curLine.length} in line.`;
+}
+
+function nowServing(curLine) {
+  curLine.
 }
